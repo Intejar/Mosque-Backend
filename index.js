787ff28103bf312434a9bc0a4609119e3d18fefb
Intejar <meherabintejar@gmail.com>
2025-03-29 05:58:41 +0600

    updated

--- index.js
+++ index.js
@@ -309,7 +309,7 @@ async function run() {
 run().catch(console.log);
 
 app.get("/", async (req, res) => {
-  res.send("Turf server is running");
+  res.send("Mosque server is running");
 });
 
 app.listen(port, () => console.log(`Mosque server is running on port ${port}`));
